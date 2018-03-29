c5570ff475dac678b2cd2e85b7c628e622282686
Dan Howe <d.howe@wrl.unsw.edu.au>
2018-03-29 17:05:18 +1100

    Fix recursive scaling bug in toSvgUnits()

--- js/canvas.js
+++ js/canvas.js
@@ -1,34 +1,21 @@
-// Set canvas size
-var canvas_w = window.innerWidth - 50;
-var canvas_h = canvas_w * 0.3;
-
-// Craete canvas
-var canvas = d3.select("#canvas").append("svg")
-  .attr("width", canvas_w)
-  .attr("height", canvas_h);
-
-
-var bathy = [
-  [0, 0],
-  [5000, 0],
-  [7500, 250],
-  [25000, 400],
-  [30000, 400],
-  [30000, 0],
-  [0, 0],
-];
+function removeByTag(tag_name) {
+  var elements = document.getElementsByTagName(tag_name);
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
 
 function redraw() {
-  var bathy_x_values = bathy.map(function(elt) {
-    return elt[0];
-  });
-  var bathy_y_values = bathy.map(function(elt) {
-    return elt[1];
-  });
-  var bathy_x_min = Math.min.apply(null, bathy_x_values);
-  var bathy_x_max = Math.max.apply(null, bathy_x_values);
-  var bathy_y_min = Math.min.apply(null, bathy_y_values);
-  var bathy_y_max = Math.max.apply(null, bathy_y_values);
+  removeByTag('svg')
+
+  var canvas_w = window.innerWidth - 50;
+  var canvas_h = canvas_w * 0.3;
+
+  // Create canvas
+  var canvas = d3.select("#canvas").append("svg")
+    .attr("id", "canvas")
+    .attr("width", canvas_w)
+    .attr("height", canvas_h);
 
   var water = [
     [0, 0],
@@ -36,6 +23,20 @@ function redraw() {
     [30000, 1000],
     [30000, 0],
   ];
+
+  var bathy = config.bathy;
+
+  var bathy_x_values = bathy.map(function(elt) {
+    return elt[0];
+  });
+  var bathy_y_values = bathy.map(function(elt) {
+    return elt[1];
+  });
+
+  var bathy_x_min = Math.min.apply(null, bathy_x_values);
+  var bathy_x_max = Math.max.apply(null, bathy_x_values);
+  var bathy_y_min = Math.min.apply(null, bathy_y_values);
+  var bathy_y_max = Math.max.apply(null, bathy_y_values);
 
   // Set wave direction
   var rtl = false;
@@ -47,14 +48,16 @@ function redraw() {
   var x_scale = canvas_w / model_w;
   var y_scale = canvas_h / model_h / 2;
 
-  function toSvgUnits(x) {
-    for (i = 0; i < x.length; i++) {
+  function toSvgUnits(pts_raw) {
+    var pts = [];
+    for (i = 0; i < pts_raw.length; i++) {
       // Flip x coordinate if required
-      x[i][0] = rtl * canvas_w - (rtl * 2 - 1) * x[i][0] * x_scale;
+      x = rtl * canvas_w - (rtl * 2 - 1) * pts_raw[i][0] * x_scale;
       // Invert y coordinate to fit SVG canvas
-      x[i][1] = canvas_h - x[i][1] * y_scale;
+      y = canvas_h - pts_raw[i][1] * y_scale;
+      pts.push([x, y]);
     }
-    return x
+    return pts;
   }
 
   // Draw water
@@ -80,16 +83,16 @@ function redraw() {
     .style("stroke", "black")
     .attr("marker-end", "url(#arrow)")
     .attr("points", toSvgUnits(dim_pts));
-
-  // Update data
-  var pts = [
-    [0, 0],
-    [0, 100],
-    [40000, 100],
-    [40000, 0],
-  ]
-
-  d3.select("#water").attr("points", toSvgUnits(pts))
+  //
+  // // Update data
+  // var pts = [
+  //   [0, 0],
+  //   [0, 100],
+  //   [40000, 100],
+  //   [40000, 0],
+  // ]
+  //
+  // d3.select("#water").attr("points", toSvgUnits(pts))
 }
 
 redraw()
